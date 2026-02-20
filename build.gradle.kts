24515f9d1813636d7f56bbbe02524a798f42ca1c
Andrew Bissell <abissell@gmail.com>
2026-02-20 03:30:05 +0000

    switch CI workflow to Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SourcesJar
+
 plugins {
     `java-library`
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 java {
@@ -8,7 +13,7 @@ java {
     }
 }
 
-version = "0.4.0"
+version = "0.7.0"
 group = "com.abissell"
 
 repositories {
@@ -32,4 +37,44 @@ tasks.withType<JavaCompile>().configureEach {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+mavenPublishing {
+    configure(JavaLibrary(
+        javadocJar = JavadocJar.Javadoc(),
+        sourcesJar = SourcesJar.Sources(),
+    ))
+
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates("com.abissell.logutil", "logutil", version.toString())
+
+    pom {
+        name.set("java-log-util")
+        description.set("A set of Java utilities for working with log4j2")
+        inceptionYear.set("2023")
+        url.set("https://github.com/abissell/java-log-util")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("abissell")
+                name.set("Andrew Bissell")
+                email.set("abissell@gmail.com")
+                url.set("https://www.abissell.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git@github.com:abissell/java-log-util.git")
+            developerConnection.set("scm:git:ssh://github.com:abissell/java-log-util.git")
+            url.set("https://github.com/abissell/java-log-util/tree/main")
+        }
+    }
 }
